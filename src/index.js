10daea13dfb544e76123351f1fc0bd79d258be12
anonto <anonto1080@gmail.com>
2024-08-09 23:16:55 +0600

    db setuped

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import cors from "cors";
 import adminRouter from './routes/admin.routes.js';
+import connectionDB from './DB/db.js';
 
 const app = express();
-
-// This name is for the database collection 
-const DB_NAME = "Porthfolio";
 
 // configuration of dotenv
 dotenv.config();
@@ -24,21 +22,15 @@ app.use(express.json({limit:"5kb"}));
 app.use(express.urlencoded({ extended: true , limit:"15kb" }));
 
 // In this line I have done the route connection
-app.use("/api/admin", adminRouter)
+app.use("/api/admin", adminRouter);
 
-app.listen(process.env.PORT || 3500 , async ()=>{
-
-    console.log(`Your server is listening at ${process.env.PORT}`);
+// I will connect the database in heare
+connectionDB()
+.then(
+    app.listen(process.env.PORT || 3500 , ()=>{
     
-    // I will connect the database in heare
-    try {
-
-        const dataBase = await mongoose.connect(`${process.env.MONGOOSE_URI}/${DB_NAME}`);
-
-        console.log(`Your database connection on ${connectionInstance.connection.host}`)
-
-    } catch (error) {
-        console.log( "You have some problem on databas" & error.message);
-        process.exit(1);
-    };
-});
+        console.log(`Your server is listening at ${process.env.PORT}`);
+    
+    })
+)
+.catch (err => console.log(err));
