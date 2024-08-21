96497f0f1d9173bbcedf3b2a6f8735dc938c81c7
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-21 12:55:10 +0100

    updated

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,4 +1,5 @@
 import { Admin } from '../model/admin.model.js';
+import { Project } from '../model/project.model.js';
 import asyncHendler from '../util/asyncHendler.js';
 
 const register = asyncHendler( async (req , res )=>{
@@ -6,7 +7,7 @@ const register = asyncHendler( async (req , res )=>{
     const { name , email , password , role } = req.boy;
 
     if(
-        [name, email, password , role].some( e => e === 'admin' )
+        [name, email, password , role].some( e => e?.trim() === '' )
     ) return res.status(401).json({message:"Please fulfill the from"});
 
     const userExid = await Admin.findOne({email});
@@ -22,9 +23,40 @@ const register = asyncHendler( async (req , res )=>{
         }
     );
 
-    if (!creatUser) return res.status(404).json({ message: "user not created" });
+    const createdUser = await Admin.findById(creatUser._id).setect("-pasword");
 
+    if(!createdUser) return res.status(400).json({message:"your user was not created" });
+
+    return res.status(201).json({message:"User created"});
+});
+
+const projectUpload = asyncHendler( async (req,res)=>{
+
+    const { title , liveLink , sorceCode } = req.boy;
+
+    const frontImage = req.file?.frontImage[0]?.path;
+
+    if(
+        [title , liveLink , sorceCode].some(e=>e?.trim() === "" ) 
+    ) return res.status(401).json({message:"please enter your all project information"});
+
+    if (!frontImage) return res.status(401).json({message:"You are facing the problem in loading the Image"});
+
+    const project = await Project.create(
+        {
+            title,
+            liveLink,
+            sorceCode,
+            frontImage
+        }
+    );
+
+    const projectCreated = await Project.findById(project._id);
+
+    if(!projectCreated) return res.status(400).json({message:"your project was not created"});
+    
+    return res.status(201).json({message:"project created", data:projectCreated});
 
 });
 
-export { register };
+export { register , projectUpload };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,7 +4,9 @@ import { upload } from '../middlewares/uploadImage.middlewares.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post("/register", upload() , register);
+adminRouter.post("/register" , register);
+
+adminRouter.post("/uploadImage" , upload.fields({name:"frontImage",maxCount:1}) , projectUpload );
 
 // adminRouter.route("/ami").post(upload);
 
