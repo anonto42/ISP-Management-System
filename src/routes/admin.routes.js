830167023d04fc7c02c1562c4b4ce1172a2da175
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-22 17:57:46 +0100

    all route added

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import { register , projectUpload } from '../controller/admin.controller.js';
+import { register , projectUpload , getProjects , login , messageCreate , getMessage } from '../controller/admin.controller.js';
 import { upload } from '../middlewares/uploadImage.middlewares.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post("/register" , register);
+adminRouter.post( "/register" , register );
 
-adminRouter.post("/uploadImage" , upload.single("frontImage") , projectUpload );
-// adminRouter.post("/uploadImage" , upload.fields([{name:"frontImage",maxCount:1}]) , projectUpload );
+adminRouter.post( "/uploadImage" , upload.single("frontImage") , projectUpload );
 
-export default adminRouter
+adminRouter.post( "/messageSend" , messageCreate );
+
+adminRouter.get( "/login" , login );
+
+adminRouter.get( "/getProjecs", getProjects );
+
+adminRouter.get( "/getMessages" , getMessage );
+
+export default adminRouter;
