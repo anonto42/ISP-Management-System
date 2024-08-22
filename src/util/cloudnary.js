3b394855842801d9f39acbe9d284ee84b4cc0a7c
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-22 13:51:03 +0100

    configaration updated

--- src/util/cloudnary.js
+++ src/util/cloudnary.js
@@ -2,43 +2,43 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 
-;(async function() {
-
+;(function() {
     // Configuration
+    // cloudinary.config({ 
+    //     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    //     api_key: process.env.CLOUDINARY_API_KEY, 
+    //     api_secret: process.env.CLOUDINARY_API_SECRET
+    // });
+
     cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET
+        cloud_name: 'ddsnont4o', 
+        api_key: '711779614851591', 
+        api_secret: 'x68yEMTpaY0pyrLXDHKZHv-XCQk'
     });
-});
+
+})();
 
 
 const uploadOnCloudinary = async (LocalFileImageOrVideo) =>{
     try {
-        if (!LocalFileImageOrVideo) return null
-
-        console.log(LocalFileImageOrVideo)
-
-        //upload the file on cloudinary
-
-        // const response = await cloudinary.uploader.upload(LocalFileImageOrVideo, {
-        //     resource_type: "auto"
-        // })
+        if (!LocalFileImageOrVideo) return console.log("File is not come on cloudinary")
 
         const response = await cloudinary.uploader.upload(LocalFileImageOrVideo,
             {
                 resource_type:"auto"
             }
         )
+        .catch(err => console.error(err))
+
+        if(!response) return console.log("File was not uploaded")
 
         fs.unlinkSync(LocalFileImageOrVideo)
-        console.log(response.url);
 
         return response;
 
     } catch (error) {
         fs.unlinkSync(LocalFileImageOrVideo);
-        return null && error.message ;
+        return error ;
     }
 }
 
