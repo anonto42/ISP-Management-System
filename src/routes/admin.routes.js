3155d617cbf15bc928c8a22a0c244aed9850dbb1
Sohidul Islam Ananto <anontom90@gmail.com>
2024-08-23 18:26:56 +0100

    updated push

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { register , projectUpload , getProjects , login , messageCreate , getMessage } from '../controller/admin.controller.js';
+import { register , projectUpload , getProjects , login , messageCreate , getMessage , logout , deleteMessage , deleteProjects } from '../controller/admin.controller.js';
 import { upload } from '../middlewares/uploadImage.middlewares.js';
+import { verifyJWT } from '../middlewares/auth.middelwares.js';
 
 const adminRouter = express.Router();
 
@@ -10,10 +11,21 @@ adminRouter.post( "/uploadImage" , upload.single("frontImage") , projectUpload )
 
 adminRouter.post( "/messageSend" , messageCreate );
 
-adminRouter.post( "/login" , login );
 
 adminRouter.get( "/getProjecs", getProjects );
 
 adminRouter.get( "/getMessages" , getMessage );
+
+// delete routes
+adminRouter.delete("/deleteProject", deleteProjects );
+
+adminRouter.delete("/deleteMessage" , deleteMessage );
+
+
+// secout routes
+adminRouter.post( "/login" , login );
+
+adminRouter.post("/logout", verifyJWT , logout)
+
 
 export default adminRouter;
