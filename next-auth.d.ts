6c86a7bd0699be24ee02690a0f502aa8e24d03a0
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-12 11:46:09 +0600

    init

--- next-auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultSession } from "next-auth";
-
-export declare module "next-auth"{
-    interface Session{
-        user:{
-            id: String;
-        } & DefaultSession["user"];
-    }
-}
