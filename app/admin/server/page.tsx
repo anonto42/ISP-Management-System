e4f20a7937877547f3d218eae763b32de54598fc
anonto42 <anonto1080@gmail.com>
2025-03-20 11:48:02 +0600

    another page

---
+++ app/admin/server/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminDeshboard = () => {
+  return (
+    <div className='w-full h-full'>
+      <h1 className='text-5xl'>server</h1>
+    </div>
+  )
+}
+
+export default AdminDeshboard
