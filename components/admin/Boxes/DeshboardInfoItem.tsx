93dd3b78ca740f92e2950ebf59808420f7c6dc83
anonto42 <anonto1080@gmail.com>
2025-03-20 10:55:00 +0600

    Single status item created

---
+++ components/admin/Boxes/DeshboardInfoItem.tsx
@@ -0,0 +1,28 @@
+import { renderData } from '@/app/admin/page'
+import React from 'react'
+
+const DeshboardInfoItem = ({data}:{data:renderData}) => {
+  return (
+    <div className='p-6 w-auto h-auto bg-[#efecec] text-black'>
+        <div className='flex gap-4 items-center'>
+            <div
+                style={{
+                    backgroundColor: data.iconBoxColor
+                }} 
+                className='w-14 h-14 flex items-center justify-center shadow-sm rounded-sm'> 
+                <data.icon
+                    style={{
+                        color: data.iconColor
+                    }} 
+                    className={`text-3xl`} />
+            </div>
+            <div className=''>
+                <h4 className='font-light'>{data.title}</h4>
+                <h2 className='text-xl font-semibold'>{data.count}</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DeshboardInfoItem
