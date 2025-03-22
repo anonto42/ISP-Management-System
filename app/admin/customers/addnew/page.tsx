0bf436d3e757976810e07f23477dc879abe5db8d
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-23 00:04:28 +0600

    new user added page created

---
+++ app/admin/customers/addnew/page.tsx
@@ -0,0 +1,14 @@
+import AddNewUser from '@/components/addClient/AddNewUser'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <section className='w-full max-w-[1600px] p-6 mx-auto'>
+        <AddNewUser />
+      </section>
+    </div>
+  )
+}
+
+export default page
