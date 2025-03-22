305012853e27d608df07a33666b1be6316500062
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-23 01:04:39 +0600

    send message individual user page created

---
+++ app/admin/message/individual/page.tsx
@@ -0,0 +1,14 @@
+import Massage from '@/components/message/Massage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className='p-6'>
+        <Massage />
+      </div>
+    </div>
+  )
+}
+
+export default page
