e6dcad33483383515ad60008d62e6eb9cccabe54
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-23 01:00:19 +0600

    send message to all user

---
+++ app/admin/message/all/page.tsx
@@ -0,0 +1,12 @@
+import Massage from '@/components/message/Massage';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className='p-6'>
+      <Massage isSendAll />
+    </div>
+  )
+}
+
+export default page
