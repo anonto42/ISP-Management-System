085a4416ea0c4b5038bb80770c03a5f5dd910d98
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-24 00:01:26 +0600

    Register reseller page

---
+++ app/admin/resellers/addnew/page.tsx
@@ -0,0 +1,14 @@
+import AddReseller from '@/components/addClient/AddReseller'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <section className='w-full max-w-[1600px] p-6 mx-auto'>
+        <AddReseller/>
+      </section>
+    </div>
+  )
+}
+
+export default page
