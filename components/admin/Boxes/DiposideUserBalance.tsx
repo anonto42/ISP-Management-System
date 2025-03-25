87fe1e2ba7369c4c7caab964963c558ab8dec770
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-25 14:07:28 +0600

    styling updated

--- components/admin/Boxes/DiposideUserBalance.tsx
+++ components/admin/Boxes/DiposideUserBalance.tsx
@@ -21,8 +21,8 @@ const DiposideUserBalance = ({
 
   return (
     <div className='z-10 absolute top-[50%] left-[50%] w-full h-full bg-[#0000002c] transform -translate-x-[50%] -translate-y-[50%] flex justify-center items-center'>
-        <div className='w-[300px] mx-auto bg-[#F7F7F7]'>
-            <div className='text-lg font-semibold p-3 border-b border-gray-300 flex items-center justify-between'>
+        <div className='w-[300px] sm:w-[500px] rounded-sm mx-auto bg-[#F7F7F7]'>
+            <div className='text-lg sm:text-xl font-semibold p-3 border-b border-gray-300 flex items-center justify-between'>
                 <h1>Diposide</h1>
                 <IoIosCloseCircleOutline
                     onClick={()=>setPosition(false)}
@@ -31,11 +31,11 @@ const DiposideUserBalance = ({
             </div>
             <div className='px-3 py-3'>
                 <input 
-                    className='set_inp_ text-sm'
+                    className='set_inp_ text-sm sm:text-lg'
                     type="text"
                     placeholder='Amount' />
                 <input 
-                    className='set_inp_ text-sm mt-3'
+                    className='set_inp_ text-sm sm:text-lg mt-3'
                     type="text"
                     placeholder='Transaction' />
                 <div
