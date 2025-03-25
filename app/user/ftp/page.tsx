9ae7cd75e2ffebd44b8085f67fac1e99d7e4d037
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-25 20:15:31 +0600

    halper payges

---
+++ app/user/ftp/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FtpServers = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default FtpServers
---
+++ app/user/pay/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PayBill = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default PayBill
