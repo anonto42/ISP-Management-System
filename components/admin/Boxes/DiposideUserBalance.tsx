84c36f39ea93320890d504702e9053be84d9ca7e
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-25 14:10:14 +0600

    width in vh

--- components/admin/Boxes/DiposideUserBalance.tsx
+++ components/admin/Boxes/DiposideUserBalance.tsx
@@ -21,7 +21,7 @@ const DiposideUserBalance = ({
 
   return (
     <div className='z-10 absolute top-[50%] left-[50%] w-full h-full bg-[#0000002c] transform -translate-x-[50%] -translate-y-[50%] flex justify-center items-center'>
-        <div className='w-[300px] sm:w-[500px] rounded-sm mx-auto bg-[#F7F7F7]'>
+        <div className='w-[35vh] sm:w-[500px] rounded-sm mx-auto bg-[#F7F7F7]'>
             <div className='text-lg sm:text-xl font-semibold p-3 border-b border-gray-300 flex items-center justify-between'>
                 <h1>Diposide</h1>
                 <IoIosCloseCircleOutline
