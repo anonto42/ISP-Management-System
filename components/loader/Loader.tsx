794a1fa40760a1f698274c6667de2ab79daa07e8
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-30 01:57:01 +0600

    new loader added

--- components/loader/Loader.tsx
+++ components/loader/Loader.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { DNA } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-opacity-75 bg-[#ffffff2f] flex justify-center items-center z-50">
-      <div className="w-16 h-16 border-4 border-t-4 border-white rounded-full animate-spin"></div>
+    <div className="fixed top-0 left-0 w-full h-full bg-opacity-75 bg-[#ffffff4b] flex justify-center items-center z-50">
+      <DNA
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+        />
     </div>
   );
 };
