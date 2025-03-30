3b95efa28481c6227305b2d86a4f09c0d61c2c21
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-30 17:58:48 +0600

    Protected only for users

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,6 +1,8 @@
 import Naveber from "@/components/Naveber/Naveber";
+import { getUser } from "@/lib/session";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { redirect } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,11 +19,17 @@ export const metadata: Metadata = {
   description: "This is a softwer for Inter net service providers. They can manage there users in hear",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const User = await getUser();
+  if (User.userType !== "user") {
+    redirect("/")
+  }
+
   return (
     <html lang="en">
       <body
