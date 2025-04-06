082bdd8f8965967b0e59dbc242db554d2acf876a
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-07 00:47:36 +0600

    extra import cleared

--- app/admin/resellers/top-up/page.tsx
+++ app/admin/resellers/top-up/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 import Loader from '@/components/loader/Loader';
 import axios, { AxiosError } from 'axios';
-import { redirect } from 'next/navigation';
 import React, { useState } from 'react'
 import { toast } from 'react-toastify';
 
-const page = () => {
+const TopUp = () => {
   const [userName, setUserName] = useState<string>("");
   const [amount, setAmout] = useState<string>("");
   const [paymentDate, setPaymentDate] = useState<Date>();
@@ -88,4 +87,4 @@ const page = () => {
   )
 }
 
-export default page
+export default TopUp
