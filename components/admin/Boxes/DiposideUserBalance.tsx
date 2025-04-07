07505cd01d44757c9a3c008342c39c6f894ce08b
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-07 16:34:51 +0600

    deposid user function added success fully

--- components/admin/Boxes/DiposideUserBalance.tsx
+++ components/admin/Boxes/DiposideUserBalance.tsx
@@ -1,7 +1,10 @@
 "use client"
+import Loader from '@/components/loader/Loader';
 import { User } from '@prisma/client';
-import React, { SetStateAction } from 'react';
+import axios, { AxiosError } from 'axios';
+import React, { SetStateAction, useState } from 'react';
 import { IoIosCloseCircleOutline } from "react-icons/io";
+import { toast } from 'react-toastify';
 
 const DiposideUserBalance = ({
     setPosition,
@@ -10,21 +13,79 @@ const DiposideUserBalance = ({
     setPosition: React.Dispatch<SetStateAction<boolean>>,
     user: User
 }) => {
+    const [loading,setLoading]=useState<boolean>(false);
+    const [amout,setAmount] = useState<string>("");
+    const [trang,setTrang] = useState<string>("");
+    const [invoiceType] = useState<string>("income")
 
-
-    const handelUpdate = async ()=>{
-        try {
-
-            setPosition(false)
+    const handerUpload = async () => {
+        if (invoiceType === "income") {
+          try {
+            setLoading(true);
+      
+            const income_data = {
+              amount:Number(amout), 
+              type:"income", 
+              purpes:trang, 
+              date: new Date(),
+              userName:user.userName,
+              receved:"Hand Cash"
+            }
+      
+            const { data } = await axios.post("/api/transection",income_data);
             
-        } catch (error) {
-            console.log(error)
+            setLoading(false);
+      
+            toast.success(data.message);
+            setPosition(false);
+      
+          } catch (error) {
+            console.log(error);
+            if (error instanceof AxiosError) {
+                toast.error(`${error.response?.data?.message}`);
+                console.log(error.response);
+            } else {
+                console.error("An unknown error occurred:", error);
+            }
+            setLoading(false);
+          }
+        }else{
+        //   try {
+        //     setLoading(true);
+      
+        //     const income_data = {
+        //       amount:Number(amout), 
+        //       type:"expense", 
+        //       purpes:purpus, 
+        //       date: date,
+        //       userName:userName,
+        //       receved
+        //     }
+      
+        //     const { data } = await axios.post("/api/transection",income_data);
+            
+        //     console.log(data);
+        //     setLoading(false);
+      
+        //     toast.success(data.message);
+        //     setAddComponent(false);
+      
+        //   } catch (error) {
+        //     console.log(error);
+        //     if (error instanceof AxiosError) {
+        //         toast.error(`${error.response?.data?.message}`);
+        //         console.log(error.response);
+        //     } else {
+        //         console.error("An unknown error occurred:", error);
+        //     }
+        //     setLoading(false);
+        //   }
         }
-    }
-
+      }
 
   return (
     <div className='z-10 absolute top-[50%] left-[50%] w-full h-full bg-[#0000002c] transform -translate-x-[50%] -translate-y-[50%] flex justify-center items-center'>
+        { loading && <Loader /> }
         <div className='w-[35vh] sm:w-[500px] rounded-sm mx-auto bg-[#F7F7F7]'>
             <div className='text-lg sm:text-xl font-semibold p-3 border-b border-gray-300 flex items-center justify-between'>
                 <h1>Diposide</h1>
@@ -37,16 +98,20 @@ const DiposideUserBalance = ({
                 <input 
                     className='set_inp_ text-sm sm:text-lg'
                     type="text"
+                    value={amout}
+                    onChange={(e)=>setAmount(e.target.value)}
                     placeholder='Amount' />
                 <input 
                     className='set_inp_ text-sm sm:text-lg mt-3'
                     type="text"
-                    placeholder='Transaction' />
+                    value={trang}
+                    onChange={(e)=>setTrang(e.target.value)}
+                    placeholder='Transaction type' />
                 <div
                     className='w-full flex justify-center pt-3'
                 >
                     <button
-                        onClick={()=>handelUpdate()}
+                        onClick={()=>handerUpload()}
                         className='px-3 py-2 bg-[green] font-semibold text-white shadow active:scale-95'
                     >Update</button>
                 </div>
