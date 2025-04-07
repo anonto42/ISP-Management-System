5f7318d4fe46f2d23e9a03f46000f12bad8ea715
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-07 16:19:43 +0600

    z-index incresed

--- components/admin/NaveBar.tsx
+++ components/admin/NaveBar.tsx
@@ -9,7 +9,7 @@ const NaveBar = () => {
   const [profileBar,setProfileBare] = useState(true);
   
   return (
-    <nav className='w-full h-[55px] bg-[#23232340] shadow absolute'>
+    <nav className='w-full h-[55px] bg-[#23232340] shadow absolute z-50'>
       <div className='w-full h-full flex justify-between items-center px-6'>
         <div></div>
         <div className='relative'>
