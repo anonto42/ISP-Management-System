1a6e8de983a63d631ad577e94c5d00dfc75dca12
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-09 01:03:02 +0600

    updated the expense page also for refresh

---
+++ app/admin/income/expense/ExpenseSection.tsx
@@ -0,0 +1,58 @@
+"use client"
+import Loader from '@/components/loader/Loader'
+import AddInvoice from '@/components/paginate/AddInvoice'
+import { Transaction } from '@prisma/client'
+import axios, { AxiosError } from 'axios'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+
+const ExpenceSection = () => {
+    const [counter,setCounter] = useState<number>(0);
+    const [loading,setLoading] = useState<boolean>(false);
+    const [currentData,setData] = useState<Transaction[]>();
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const { data } = await axios.post(`/api/oparation`,{schemaName:"transaction"},{withCredentials:true});
+                setData(data.data);
+                setLoading(false);
+            } catch (error) {
+                if(error instanceof AxiosError){
+                    toast.error("Error fetching user data")
+                }
+            }
+        }
+        fetchData();
+    },[counter]);
+
+    const paymentHistory = currentData?.filter( i => i.transactionType === "expense").map(data => ({
+      userName: data.userName,
+      amount: data.amount,
+      transactionType: data.transactionType,
+      puspes: data.puspes,
+      modelName:"transaction",
+      id:data.id,
+      date: new Date(data.date).toISOString().split("T")[0]
+    })).reverse();
+      
+
+  return (
+    <section className='w-full max-w-[1600px] p-6 relative'>
+      { loading && <Loader/> }
+      <AddInvoice
+        action={{delete:true}}
+        addNewInvoice
+        invoiceType='expense'
+        allData={paymentHistory || []}
+        fields={["User Name","Amount","Transaction Type","Purpes","Date"]}
+        paginateTitle='Expense'
+        refresh={setCounter}
+      />
+    </section>
+  )
+}
+
+export default ExpenceSection
--- app/admin/income/expense/page.tsx
+++ app/admin/income/expense/page.tsx
@@ -1,30 +1,10 @@
-import AddInvoice from '@/components/paginate/AddInvoice'
-import prismaDB from '@/prisma/pot';
 import React from 'react'
-
-const paymentHistory = (await prismaDB.transaction.findMany()).filter( i => i.transactionType === "expense").map(data => ({
-  userName: data.userName,
-  amount: data.amount,
-  transactionType: data.transactionType,
-  puspes: data.puspes,
-  modelName:"transaction",
-  id:data.id,
-  date: new Date(data.date).toISOString().split("T")[0]
-})).reverse();
+import ExpenceSection from './ExpenseSection';
 
 const page = () => {
   return (
     <div className='w-full h-full'>
-      <section className='w-full max-w-[1600px] p-6 relative'>
-        <AddInvoice
-          action={{delete:true}}
-          addNewInvoice
-          invoiceType='expense'
-          allData={paymentHistory}
-          fields={["User Name","Amount","Transaction Type","Purpes","Date"]}
-          paginateTitle='Expense'
-        />
-      </section>
+      <ExpenceSection />
     </div>
   )
 }
