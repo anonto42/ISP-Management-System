0e4cd2aad4342f592416bfbe6675602b429d138d
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-09 01:09:41 +0600

    new component added for the refres on delete

---
+++ app/admin/resellers/view/ViewPage.tsx
@@ -0,0 +1,60 @@
+"use client"
+import Loader from '@/components/loader/Loader'
+import AddInvoice from '@/components/paginate/AddInvoice'
+import PaginationComponent from '@/components/paginate/Paginate'
+import { User } from '@prisma/client'
+import axios, { AxiosError } from 'axios'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+
+const ViewSection = () => {
+    const [counter,setCounter] = useState<number>(0);
+    const [loading,setLoading] = useState<boolean>(false);
+    const [currentData,setData] = useState<User[]>();
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+
+                const { data } = await axios.post(`/api/oparation`,{schemaName:"user"},{withCredentials:true});
+                setData(data.data);
+                setLoading(false);
+      
+            } catch (error) {
+                if(error instanceof AxiosError){
+                    toast.error("Error fetching user data")
+                }
+            }
+        }
+        fetchData();
+    },[counter]);
+
+      const resellers = currentData?.filter( user => user.userType === "reseller").map(user => ({
+        userName: user.userName,
+        mobile: user.phoneNumber,
+        email: user.email,
+        modelName:"user",
+        id:user.id,
+        balance: user.balance,
+        joiningDate: new Date(user.dateOfConnection).toISOString().split("T")[0]
+      }))
+      
+
+  return (
+    <section className='w-full max-w-[1600px] p-6 mx-auto'>
+        { loading && <Loader /> }
+        <PaginationComponent
+          paginateTitle='All Reseller'
+          action={{delete:true}}
+          fields={["User name","Mobile","Email","Balance","Joining Date"]}
+          addUserButton={false}
+          allData={resellers || []}
+          refresher={setCounter}
+        />
+    </section>
+  )
+}
+
+export default ViewSection
--- app/admin/resellers/view/page.tsx
+++ app/admin/resellers/view/page.tsx
@@ -1,32 +1,11 @@
-import PaginationComponent from '@/components/paginate/Paginate';
-import prismaDB from '@/prisma/pot';
 import React from 'react';
-
+import ViewSection from './ViewPage';
 
 const page = async () => {
-  
-  const users = await prismaDB.user.findMany();
-  const resellers = users.filter( user => user.userType === "reseller").map(user => ({
-    userName: user.userName,
-    mobile: user.phoneNumber,
-    email: user.email,
-    modelName:"user",
-    id:user.id,
-    balance: user.balance,
-    joiningDate: new Date(user.dateOfConnection).toISOString().split("T")[0]
-  }))
 
   return (
     <div className='w-full h-full'>
-      <section className='w-full max-w-[1600px] p-6 mx-auto'>
-        <PaginationComponent
-          paginateTitle='All Reseller'
-          action={{delete:true,edite:true}}
-          fields={["User name","Mobile","Email","Balance","Joining Date"]}
-          addUserButton={false}
-          allData={resellers}
-        />
-      </section>
+      <ViewSection />
     </div>
   )
 }
