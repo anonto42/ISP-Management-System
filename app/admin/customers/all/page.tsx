c23b8eda334589a521a01705b537b6b4b0f6eacb
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-09 00:34:28 +0600

    simplyfy the page

--- app/admin/customers/all/page.tsx
+++ app/admin/customers/all/page.tsx
@@ -1,40 +1,11 @@
-import PaginationComponent from '@/components/paginate/Paginate';
-import prismaDB from '@/prisma/pot';
 import React from 'react';
-
-
-interface user{
-  userName:string;
-  password:string;
-  mobile:string;
-  clientInfo:string;
-  expire:string;
-}
+import Section from './Section';
 
 const page = async () => {
 
-  const allUser = await prismaDB.user.findMany();
-
-  const formattedUsers:user[] = allUser.filter( doc => doc.userType !== "admin" ).map(user => ({
-    userName: user.userName,
-    password: user.password,
-    mobile: user.phoneNumber,
-    clientInfo: `${user.district}, ${user.upozala}`, // You can adjust this as needed
-    expire:user.expireDate == null ? "0.0.0.0" :user.expireDate?.toLocaleDateString(), // Format the date as needed
-  }));
-
   return (
     <div className='w-full h-full'>
-      <section className='w-full max-w-[1600px] p-6 mx-auto'>
-        <PaginationComponent<user>
-          paginateTitle='All Customers'
-          action={{view:true,delete:true,edite:true}}
-          fields={["User Name","password","Mobile","Client Info","Expire"]}
-          allData={formattedUsers}
-          addUserButton
-          key={"All cuntomers"}
-        />
-      </section>
+      <Section/>
     </div>
   )
 }
