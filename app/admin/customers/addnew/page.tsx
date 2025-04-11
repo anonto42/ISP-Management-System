14d9fea81c3ca1da18883626dabf164880c9fcfd
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-11 23:17:08 +0600

    get the acctive packages

--- app/admin/customers/addnew/page.tsx
+++ app/admin/customers/addnew/page.tsx
@@ -1,11 +1,13 @@
 import AddNewUser from '@/components/addClient/AddNewUser'
+import prismaDB from '@/prisma/pot'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  const packagesFromDB = await prismaDB.packages.findMany();
   return (
     <div>
       <section className='w-full max-w-[1600px] p-6 mx-auto'>
-        <AddNewUser />
+        <AddNewUser packages={packagesFromDB} />
       </section>
     </div>
   )
